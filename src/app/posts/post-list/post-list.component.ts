d57213cda20cfcbe06f984522d82231c44a55424
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-09-30 19:32:52 -0300

    binding between components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,9 +6,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
+  /*posts = [
     { title: 'First Post', content: 'this is the first post\'s content'},
     { title: 'Second Post', content: 'this is the second post\'s content'},
     { title: 'Third Post', content: 'this is the third post\'s content'},
-  ];
+  ];*/
+  @Input()
+  posts = [];
 }
