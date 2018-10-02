37e2932d681d84d2119cf9a2f10140892988648b
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-01 22:22:45 -0300

    post service call api endpoint

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,18 +11,13 @@ import { PostService } from '../post.service';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  /*posts = [
-    { title: 'First Post', content: 'this is the first post\'s content'},
-    { title: 'Second Post', content: 'this is the second post\'s content'},
-    { title: 'Third Post', content: 'this is the third post\'s content'},
-  ];*/
   posts: Post[] = [];
   private postsSub: Subscription;
 
   constructor(public postService: PostService) {}
 
   ngOnInit(): void {
-    this.posts = this.postService.getPosts();
+    this.postService.getPosts();
     this.postsSub = this.postService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
@@ -9,9 +10,14 @@ export class PostService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    // get a copy from the object
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -20,6 +26,7 @@ export class PostService {
 
   addPost(title: string, content: string) {
     const post: Post = {
+      id: null,
       title: title,
       content: content
     };
