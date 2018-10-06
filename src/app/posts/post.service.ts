8204c5cd2f6613e97f46f8da0ace441ec1cd0ea1
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-05 21:13:03 -0300

    added edit page

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -34,6 +34,11 @@ export class PostService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    // return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{ _id: string, title: string, content: string }>('http://localhost:3000/api/posts/' + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {
       id: null,
@@ -45,6 +50,22 @@ export class PostService {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content
+    };
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
