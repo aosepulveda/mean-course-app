ec7fb6a3d2dd3155527e40409c3f0b19a43c34bd
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-21 12:07:48 -0300

    add authenticated status to header component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -28,6 +34,7 @@ export class AuthService {
     this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
+        this.authStatusListener.next(true);
       });
   }
 }
